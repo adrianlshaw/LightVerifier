1ba7378306f2dc5ebae2e99adb5f081b3d278ab6
Adrian L. Shaw <adrianlshaw@gmail.com>
2017-01-12 21:40:18 +0000

    Let's try an approach without static files

--- tests/test-script.sh
+++ tests/test-script.sh
@@ -1,13 +1,25 @@
 #/bin/bash
 
 # Add "localhost" AIK to to the trust store
-redis-cli --raw -n 13 set localhost a7ca3d9fed8e1020770622d8bf2396274c608e78
+#redis-cli --raw -n 13 set localhost a7ca3d9fed8e1020770622d8bf2396274c608e78
 
-redis-cli -h localhost -n 15 RPUSH "a7ca3d9fed8e1020770622d8bf2396274c608e78" "MTAgY2U4YmY0MTFhM2IzNWZjZmI0NTE3MjJmMTQyNzhlZGMzNmQxNDExZiBpbWEtbmcgc2hhMTpiNTI1MGVhNDZmNTFiMDUyMjVkM2FmMmU0MzY3ZTRmMWQwNzdmYzBmIGJvb3RfYWdncmVnYXRlCg=="
-redis-cli -h localhost -n 15 RPUSH "a7ca3d9fed8e1020770622d8bf2396274c608e78" "ADZRVVQyLk499gAAAADCCEAAAAAAAP////8AAwAEAAG+m+JHRZ+NT2PnVhT9Xoy4F2KsSA=="
-redis-cli -h localhost -n 15 RPUSH "a7ca3d9fed8e1020770622d8bf2396274c608e78" "MEQwN0FFNEZDMUVCQzEyMDRCNDg5ODdEQzUzODJFODI1RjZFMUI3Rgo="
-redis-cli -h localhost -n 15 RPUSH "a7ca3d9fed8e1020770622d8bf2396274c608e78" "IIBLAIBAQIBAgIEAAABHASCARwAAAABAAEAAgAAAAwAAAgAAAAAAgAAAAAAAAEAlHgy8TDVt6jn7jIEXT0hLXt3umK/JWD8daCWBUHX26qtv4hQQIHkxJZFGRW7KiA78Sfj+1RcgZP8puIjw+3NWaE0RDUlhMmi0K+HpR2LII3pbLqkmkUB14fOyhMJg/USTLOHbjuaBd7Z+Po9e7WQbCL2fARdKd8jxo9IBOBGFybHNqKYtwE+yyf3hLftMtpD0Qlw6Q2/3OT5huDd7kYgyF/IglL3fG6TTp4locvXlNNbgRorfMZQ504QdY+Ql1uXzWhAwnIx/hE8Gjez8Twqq+xYb2lnHggFr44BLPP4HniKYKuxS2/ZXTNlx2YkSFSk+ONe+3YoB9IZma56Zdf72w=="
+#redis-cli -h localhost -n 15 RPUSH "a7ca3d9fed8e1020770622d8bf2396274c608e78" "MTAgY2U4YmY0MTFhM2IzNWZjZmI0NTE3MjJmMTQyNzhlZGMzNmQxNDExZiBpbWEtbmcgc2hhMTpiNTI1MGVhNDZmNTFiMDUyMjVkM2FmMmU0MzY3ZTRmMWQwNzdmYzBmIGJvb3RfYWdncmVnYXRlCg=="
+#redis-cli -h localhost -n 15 RPUSH "a7ca3d9fed8e1020770622d8bf2396274c608e78" "ADZRVVQyLk499gAAAADCCEAAAAAAAP////8AAwAEAAG+m+JHRZ+NT2PnVhT9Xoy4F2KsSA=="
+#redis-cli -h localhost -n 15 RPUSH "a7ca3d9fed8e1020770622d8bf2396274c608e78" "MEQwN0FFNEZDMUVCQzEyMDRCNDg5ODdEQzUzODJFODI1RjZFMUI3Rgo="
+#redis-cli -h localhost -n 15 RPUSH "a7ca3d9fed8e1020770622d8bf2396274c608e78" "IIBLAIBAQIBAgIEAAABHASCARwAAAABAAEAAgAAAAwAAAgAAAAAAgAAAAAAAAEAlHgy8TDVt6jn7jIEXT0hLXt3umK/JWD8daCWBUHX26qtv4hQQIHkxJZFGRW7KiA78Sfj+1RcgZP8puIjw+3NWaE0RDUlhMmi0K+HpR2LII3pbLqkmkUB14fOyhMJg/USTLOHbjuaBd7Z+Po9e7WQbCL2fARdKd8jxo9IBOBGFybHNqKYtwE+yyf3hLftMtpD0Qlw6Q2/3OT5huDd7kYgyF/IglL3fG6TTp4locvXlNNbgRorfMZQ504QdY+Ql1uXzWhAwnIx/hE8Gjez8Twqq+xYb2lnHggFr44BLPP4HniKYKuxS2/ZXTNlx2YkSFSk+ONe+3YoB9IZma56Zdf72w=="
 
+echo "Deleting List from the verifier"
+redis-cli --raw -n 15 LTRIM "$HASHAIK" '-1' '0'
+redis-cli --raw -n 15 DEL "$HASHAIK"
+
+echo "Inserting golden measurements"
+
+cat refLog | base64 -e | redis-cli --raw -n 15 LINDEX "$HASHAIK" '0' 
+cat hashPCR | base64 -e | redis-cli --raw -n 15 LINDEX "$HASHAIK" '1'
+cat pcrValue | base64 -e | redis-cli --raw -n 15 LINDEX "$HASHAIK" '2'
+cat pubAIK | base64 -e | redis-cli --raw -n 15 LINDEX "$HASHAIK" '3'
+
+echo "Done"
 
 # Start the remote attestation agent
 ./ra-agent.sh tests/a7ca3d9fed8e1020770622d8bf2396274c608e78/pubAIK --testmode 5000 10 &
