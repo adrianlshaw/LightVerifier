b73147550b01d4a30f08317dc8fcee45890f69fc
Adrian L. Shaw <adrianlshaw@gmail.com>
2017-01-17 18:31:16 +0000

    Cleaned up code in test script to use suitable variables

--- tests/test-script.sh
+++ tests/test-script.sh
@@ -4,20 +4,24 @@ pkill lqr.sh
 pkill nc.traditional
 pkill nc
 
+# Redis DB numbers
+REDIS_AIK=13
+REDIS_AIK_INFO=15
+
 HASHAIK="a7ca3d9fed8e1020770622d8bf2396274c608e78"
 
 # Add "localhost" public AIK to to the trust store
-redis-cli --raw -n 13 set "localhost" "$HASHAIK"
+redis-cli --raw -n $REDIS_AIK set "localhost" "$HASHAIK"
 
-# Remove old information from the verifier 
-redis-cli --raw -n 15 LTRIM "$HASHAIK" '-1' '0'
-redis-cli --raw -n 15 DEL "$HASHAIK"
+# Remove old information from the verifier
+redis-cli --raw -n $REDIS_AIK_INFO LTRIM "$HASHAIK" '-1' '0'
+redis-cli --raw -n $REDIS_AIK_INFO DEL "$HASHAIK"
 
 echo "Provisioning expected boot aggregate PCR and public AIK"
-redis-cli --raw -n 15 RPUSH "$HASHAIK" "$(cat tests/refLog | base64)"
-redis-cli --raw -n 15 RPUSH "$HASHAIK" "$(cat tests/hashPCR | base64)"
-redis-cli --raw -n 15 RPUSH "$HASHAIK" "$(cat tests/pcrValue | base64)"
-redis-cli --raw -n 15 RPUSH "$HASHAIK" "$(cat tests/pubAIK | base64)"
+redis-cli --raw -n $REDIS_AIK_INFO RPUSH "$HASHAIK" "$(cat tests/refLog | base64)"
+redis-cli --raw -n $REDIS_AIK_INFO RPUSH "$HASHAIK" "$(cat tests/hashPCR | base64)"
+redis-cli --raw -n $REDIS_AIK_INFO RPUSH "$HASHAIK" "$(cat tests/pcrValue | base64)"
+redis-cli --raw -n $REDIS_AIK_INFO RPUSH "$HASHAIK" "$(cat tests/pubAIK | base64)"
 
 echo "Starting agent and verification server"
 
