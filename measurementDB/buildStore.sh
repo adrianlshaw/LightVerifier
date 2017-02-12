330bcbe63b9c0c138993e1558ea090fa30b845e2
Adrian L. Shaw <adrianlshaw@gmail.com>
2017-02-12 02:09:00 +0000

    Renamed measurement scripts with more meaningful names

--- measurementDB/buildStore.sh
+++ measurementDB/buildStore.sh
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-./downloadPkgs.sh
+./download-base-pkgs.sh
 
 ./measure-executables scan_files
 
